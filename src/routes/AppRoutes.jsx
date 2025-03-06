79f8dda60463229f894f8fa4b4188758d0550698
Gabriel Pires de Camargo <72245726+gabrielpirescamargo@users.noreply.github.com>
2025-03-05 21:43:31 -0300

    remove routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserProfile from "../pages/UserProfile";
 
 const AppRoutes = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<UserProfile />} />
-    </Routes>
-  </Router>
+  <UserProfile />
+  // <Router>
+  //   <Routes>
+  //     <Route path="/" element={<UserProfile />} />
+  //   </Routes>
+  // </Router>
 );
 
 export default AppRoutes;
